8b365fc7277b9a865b63956722a24fa58512e462
Isuru <isuru@techbsl.com>
2023-09-06 23:02:41 +0530

    init code

---
+++ src/main/java/com/example/springboot3logging/SpringBoot3LoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3LoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3LoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot3logging/SpringBoot3LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3LoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
